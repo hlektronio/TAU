aaf43b01728a05694b3bbaed242a4d2db5cfd216
hlektronio <electra.saranti@gmail.com>
2020-05-22 11:58:19 +0300

    first commit

---
+++ src/chapter2exercise/MadLibs.java
@@ -0,0 +1,23 @@
+package chapter2exercise;
+
+import java.util.Scanner;
+
+public class MadLibs {
+
+    public static void main(String arg[]){
+
+        //ask season of year
+
+        System.out.println("Please tell what season it is");
+        Scanner scanner = new Scanner(System.in);
+        String season = scanner.next();
+
+        //ask for a number
+        System.out.println("Please give me a number");
+        int number = scanner.nextInt();
+
+        //output
+        System.out.println("On a "+season+" day, I drink a minimum of "+number+ " cups of coffee");
+
+    }
+}
